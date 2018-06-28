666f25e960c6a5bb125127974a79fd35a321e190
jerauld <25723428+jerauld@users.noreply.github.com>
2018-06-28 02:08:43 -0700

    adding build to heroku, modified server for heroku

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 
 mongoose.Promise = Promise;
-mongoose.connect('mongodb://localhost/nytreact');
+mongoose.connect(MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/nytreact");
 
 // Define middleware here
 app.use(bodyParser.urlencoded({ extended: true }));
